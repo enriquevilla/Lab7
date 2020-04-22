19c8817e6d0cd21596aae1131bd486882973b587
Enrique Villa <enrique.villa97@hotmail.com>
2020-04-22 12:53:56 -0500

    Move initial data to separate file

--- server.js
+++ server.js
@@ -9,29 +9,7 @@ app.use(morgan("dev"));
 const authValidation = require("./middleware/authValidation");
 app.use(authValidation);
 
-let bookmarks = [
-    {
-        id: uuid.v4(),
-        title: "Bookmark 1",
-        description: "First bookmark",
-        url: "https://www.google.com",
-        rating: 7
-    },
-    {
-        id: uuid.v4(),
-        title: "Bookmark 2",
-        description: "Second bookmark",
-        url: "https://www.facebook.com",
-        rating: 1
-    },
-    {
-        id: uuid.v4(),
-        title: "Bookmark 2",
-        description: "Third bookmark",
-        url: "https://www.twitter.com",
-        rating: 9
-    }
-];
+let bookmarks = require("./data");
 
 app.get('/bookmarks', (req, res) => {
     return res.status(200).json(bookmarks);
